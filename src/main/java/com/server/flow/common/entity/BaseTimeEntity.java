ca5db3ec4cf8752352163dee35ad247b3e215e85
minisyu <iswater.kim@gmail.com>
2024-08-23 14:15:15 +0900

    feat: add BaseTimeEntity for automatic auditing

---
+++ src/main/java/com/server/flow/common/entity/BaseTimeEntity.java
@@ -0,0 +1,23 @@
+package com.server.flow.common.entity;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
+import org.springframework.data.jpa.domain.support.AuditingEntityListener;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.EntityListeners;
+import jakarta.persistence.MappedSuperclass;
+
+@MappedSuperclass
+@EntityListeners(AuditingEntityListener.class)
+public class BaseTimeEntity {
+	@CreatedDate
+	@Column(updatable = false, nullable = false)
+	private LocalDateTime createdAt;
+
+	@LastModifiedDate
+	@Column(updatable = false, nullable = false)
+	private LocalDateTime updatedAt;
+}
